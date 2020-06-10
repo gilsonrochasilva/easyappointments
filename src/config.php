2b2b31fb9ec5640a445983fc6e045f7b4f8795f8
Gilson Rocha Silva <gilson.silva@sofintech.com.br>
2020-06-10 06:32:47 -0300

    DEBUG MODE OFF

--- src/config.php
+++ src/config.php
@@ -32,7 +32,7 @@ class Config {
 
     const BASE_URL      = 'http://localhost';
     const LANGUAGE      = 'portuguese-br';
-    const DEBUG_MODE    = TRUE;
+    const DEBUG_MODE    = FALSE;
 
     // ------------------------------------------------------------------------
     // DATABASE SETTINGS
